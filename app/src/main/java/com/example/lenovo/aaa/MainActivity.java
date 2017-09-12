cd64315d549705a8432c9c2e502d54b24135968e
gitxiaohong <ylhm-hong@163.com>
2017-09-12 11:04:29 +0800

    更改

--- app/src/main/java/com/example/lenovo/aaa/MainActivity.java
+++ app/src/main/java/com/example/lenovo/aaa/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.lenovo.aaa;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_one;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        btn_one = (Button) findViewById(R.id.btn_one);
+
+        btn_one.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_one:
+
+                break;
+        }
     }
 }
